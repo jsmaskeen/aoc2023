a20bfc9d97e95473b7c6589c2e9356b75c45d81c
jsmaskeen <jsmaskeen@gmail.com>
2023-12-14 16:10:22 +0530

    day 2

--- day02/day2.py
+++ day02/day2.py
@@ -1,16 +1,83 @@
-inp = open('test.txt').read()
-
 class Day2:
-    def __init__(self,test,inp,run_test=True) -> None:
-        self.test = open(test).read()
-        self.input = open(inp).read()
+    def __init__(self,test,inp,run_test=False) -> None:
+        self.test = open(test).read().split('\n')
+        self.input = open(inp).read().split('\n')
         self.run_test = run_test
     
     def part1(self,inp=None):
         if not inp:inp=self.input
+        p = 0
+        for j in inp:
+            o = j.split(':')
+            gid = o[0].split(' ')[-1]
+            possible = True
+            vals = o[1].strip().split(';')
+            #r,g,b
+            r = 0
+            g = 0
+            b = 0
+            for v in vals:
+                args = v.split(',')
+                for a in args:
+                    vv = a.strip().split(' ')
+                    if vv[1] == 'green':
+                        g = int(vv[0])
+                    if vv[1] == 'blue':
+                        b = int(vv[0])
+                    if vv[1] == 'red':
+                        r = int(vv[0])
+                    
+                    if (r>12) or (g>13) or (b>14):
+                        possible = False
+                r,b,g = [0]*3
+
+            if possible:
+                p+=int(gid)
+
+        print('Part1: '+ str(p))
 
     def part2(self,inp=None):
         if not inp:inp=self.input
+        cleaned = {}
+        p = 0
+        for j in inp:
+            o = j.split(':')
+            gid = o[0].split(' ')[-1]
+            vals = o[1].strip().split(';')
+            #r,g,b
+            r = 0
+            g = 0
+            b = 0
+            tmp = []
+            for v in vals:
+                args = v.split(',')
+                for a in args:
+                    vv = a.strip().split(' ')
+                    if vv[1] == 'green':
+                        g = int(vv[0])
+                    if vv[1] == 'blue':
+                        b = int(vv[0])
+                    if vv[1] == 'red':
+                        r = int(vv[0])
+                
+                tmp.append([r,g,b])
+                r,b,g = [0]*3
+
+            mr,mb,mg = [0]*3
+            # print(tmp)
+            for i in tmp:
+                r,g,b = i
+                # print(r,g,b)
+                mr = max(mr,r)
+                mg = max(mg,g)
+                mb = max(mb,b)
+                # print(mr,mg,mb)
+
+            p+= mr*mg*mb
+            
+            cleaned|={int(gid):tmp}
+
+        print('Part2: '+ str(p))
 
     def tester(self):
         print('TEST'.center(16,'#'))
@@ -23,13 +90,14 @@ class Day2:
     def solve(self):
         if self.run_test:
             self.tester()
-        self.part1()
-        print()
-        self.part2()
+        else:
+            self.part1()
+            print()
+            self.part2()
 
 
     
 
 
-solver = Day2('test.txt','input.txt')
+solver = Day2('test.txt','input.txt',False)
 solver.solve()
