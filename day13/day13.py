b850f39f13d480ee771aea3737e261c1139e1d6d
jsmaskeen <jsmaskeen@gmail.com>
2023-12-14 15:43:18 +0530

    adding templates

---
+++ day13/day13.py
@@ -0,0 +1,35 @@
+inp = open('test.txt').read()
+
+class Day13:
+    def __init__(self,test,inp,run_test=True) -> None:
+        self.test = open(test).read()
+        self.input = open(inp).read()
+        self.run_test = run_test
+    
+    def part1(self,inp=None):
+        if not inp:inp=self.input
+
+    def part2(self,inp=None):
+        if not inp:inp=self.input
+
+    def tester(self):
+        print('TEST'.center(16,'#'))
+        self.part1(self.test)
+        print()
+        self.part2(self.test)
+        print('#'*16)
+        print()
+
+    def solve(self):
+        if self.run_test:
+            self.tester()
+        self.part1()
+        print()
+        self.part2()
+
+
+    
+
+
+solver = Day13('test.txt','input.txt')
+solver.solve()
